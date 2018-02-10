d524ffeb0eb23d0e891499926de2996ec252f0e9
Brecht Dierckx <brecht.dierckx@gmail.com>
2018-02-10 14:10:53 +0100

    customizezd semantic versioning

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
@@ -1,7 +1,4 @@
 # -*- coding: utf-8 -*-
-
-__author__ = '{{ cookiecutter.full_name }}'
-__email__ = '{{ cookiecutter.email }}'
 __version__ = '{{ cookiecutter.version }}'
 
 from {{cookiecutter.project_slug}} import {{(cookiecutter.project_slug.title()).replace('_', '')}}
