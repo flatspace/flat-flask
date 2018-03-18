081df60dc30d03f9b1216a06d7a72ba47448902b
Brecht Dierckx <brecht.dierckx@restore.energy>
2018-03-18 15:35:38 +0100

    remove crud

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/controllers.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/controllers.py
@@ -1,11 +1,11 @@
-#import logging
+import logging
 
 from flask import request
 
-#LOGGER = logging.getLogger(__name__)
+LOGGER = logging.getLogger(__name__)
 
 
 def hello_world(message):
-#    LOGGER.info(request.headers)
+    LOGGER.info(request.headers)
     # do something
     return 'Hello World. {}'.format(message), 200
--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 import connexion
-from {{ cookiecutter.project_slug }} import log_config
+import log_config
 
-app = connexion.App(__name__, specification_dir='swagger')
+app = connexion.App(__name__, swagger_url="specs", specification_dir=".")
 app.add_api('{{cookiecutter.project_slug}}_api.yaml')
 
 if __name__ == '__main__':
     log_config.setup(app, "{{cookiecutter.project_slug}}", None)
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
